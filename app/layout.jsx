8759aab0d05dceb7e38d769d826392652ea294eb
Anky <ann-cathrin.konrad@capsloq.de>
2022-11-14 20:42:53 +0100

    suchergebnisse

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
+        {/*mantine*/}
         <RootStyleRegistry>
           <RootNavigation />
           {children}
